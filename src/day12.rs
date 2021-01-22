180c242299c4ae4d0e947e7e75b97efb5d466af0
Arnold Szederjesi <szederjesiarnold@gmail.com>
2021-01-22 18:38:01 +0200

    Add some more inputs, refactor

--- src/day12.rs
+++ src/day12.rs
@@ -4,7 +4,7 @@ use std::f64::consts::PI;
 pub fn part1(inp: String) {
     let mut ship = Ship1 {
         pos: Complex::new(0, 0),
-        dir: Complex::new(-1, 0),
+        dir: Complex::new(1, 0),
     };
     ship = apply_commands1(ship, inp.lines().map(&str::to_string).collect());
     println!("{}", ship.pos.re.abs() + ship.pos.im.abs())
@@ -13,7 +13,7 @@ pub fn part1(inp: String) {
 pub fn part2(inp: String) {
     let mut ship = Ship2 {
         pos: Complex::new(0, 0),
-        waypoint: Complex::new(-10, 1),
+        waypoint: Complex::new(10, 1),
     };
     ship = apply_commands2(ship, inp.lines().map(&str::to_string).collect());
     println!("{}", ship.pos.re.abs() + ship.pos.im.abs())
@@ -38,19 +38,19 @@ fn apply_commands1(mut ship: Ship1, commands: Vec<String>) -> Ship1 {
                 );
             }
             ("E", value_str) => {
+                ship.pos = move_pos1(ship.pos, Complex::new(value_str.parse::<i32>().unwrap(), 0));
+            }
+            ("W", value_str) => {
                 ship.pos = move_pos1(
                     ship.pos,
                     Complex::new(-value_str.parse::<i32>().unwrap(), 0),
                 );
             }
-            ("W", value_str) => {
-                ship.pos = move_pos1(ship.pos, Complex::new(value_str.parse::<i32>().unwrap(), 0));
-            }
             ("L", value_str) => {
-                ship.dir = turn_direction1(ship.dir, -value_str.parse::<i32>().unwrap());
+                ship.dir = turn_direction(ship.dir, value_str.parse::<i32>().unwrap());
             }
             ("R", value_str) => {
-                ship.dir = turn_direction1(ship.dir, value_str.parse::<i32>().unwrap());
+                ship.dir = turn_direction(ship.dir, -value_str.parse::<i32>().unwrap());
             }
             ("F", value_str) => {
                 ship.pos = move_pos1(ship.pos, ship.dir.scale(value_str.parse::<i32>().unwrap()));
@@ -63,37 +63,6 @@ fn apply_commands1(mut ship: Ship1, commands: Vec<String>) -> Ship1 {
 
 fn move_pos1(pos: Complex<i32>, delta_pos: Complex<i32>) -> Complex<i32> {
     pos + delta_pos
-}
-
-fn turn_direction1(dir: Complex<i32>, degree: i32) -> Complex<i32> {
-    assert_eq!(0, degree % 90);
-    let float_dir = Complex::new(dir.re as f64, dir.im as f64);
-    let result = float_dir * Complex::from_polar(1.0, (degree as f64) * PI / 180.0);
-    Complex::new(result.re.round() as i32, result.im.round() as i32)
-}
-
-#[test]
-fn test_turn_direction1() {
-    assert_eq!(
-        Complex::new(0, -1),
-        turn_direction1(Complex::new(-1, 0), 90)
-    );
-    assert_eq!(
-        Complex::new(1, 0),
-        turn_direction1(Complex::new(-1, 0), 180)
-    );
-    assert_eq!(
-        Complex::new(0, 1),
-        turn_direction1(Complex::new(-1, 0), 270)
-    );
-    assert_eq!(
-        Complex::new(0, 1),
-        turn_direction1(Complex::new(-1, 0), -90)
-    );
-    assert_eq!(
-        Complex::new(1, 0),
-        turn_direction1(Complex::new(-1, 0), -180)
-    );
 }
 
 #[test]
@@ -141,20 +110,20 @@ fn apply_commands2(mut ship: Ship2, commands: Vec<String>) -> Ship2 {
             ("E", value_str) => {
                 ship.waypoint = move_waypoint(
                     ship.waypoint,
-                    Complex::new(-value_str.parse::<i32>().unwrap(), 0),
+                    Complex::new(value_str.parse::<i32>().unwrap(), 0),
                 );
             }
             ("W", value_str) => {
                 ship.waypoint = move_waypoint(
                     ship.waypoint,
-                    Complex::new(value_str.parse::<i32>().unwrap(), 0),
+                    Complex::new(-value_str.parse::<i32>().unwrap(), 0),
                 );
             }
             ("L", value_str) => {
-                ship.waypoint = turn_direction2(ship.waypoint, -value_str.parse::<i32>().unwrap());
+                ship.waypoint = turn_direction(ship.waypoint, value_str.parse::<i32>().unwrap());
             }
             ("R", value_str) => {
-                ship.waypoint = turn_direction2(ship.waypoint, value_str.parse::<i32>().unwrap());
+                ship.waypoint = turn_direction(ship.waypoint, -value_str.parse::<i32>().unwrap());
             }
             ("F", value_str) => {
                 ship.pos = move_pos2(ship.pos, ship.waypoint, value_str.parse::<i32>().unwrap());
@@ -173,38 +142,22 @@ fn move_pos2(pos: Complex<i32>, waypoint: Complex<i32>, times: i32) -> Complex<i
     pos + waypoint.scale(times)
 }
 
-fn turn_direction2(waypoint: Complex<i32>, degree: i32) -> Complex<i32> {
+fn turn_direction(dir: Complex<i32>, degree: i32) -> Complex<i32> {
     assert_eq!(0, degree % 90);
-    let float_dir = Complex::new(waypoint.re as f64, waypoint.im as f64);
+    let float_dir = Complex::new(dir.re as f64, dir.im as f64);
     let result = float_dir * Complex::from_polar(1.0, (degree as f64) * PI / 180.0);
     Complex::new(result.re.round() as i32, result.im.round() as i32)
 }
 
 #[test]
-fn test_turn_direction2() {
-    assert_eq!(
-        Complex::new(0, -1),
-        turn_direction1(Complex::new(-1, 0), 90)
-    );
-    assert_eq!(
-        Complex::new(1, 0),
-        turn_direction1(Complex::new(-1, 0), 180)
-    );
-    assert_eq!(
-        Complex::new(0, 1),
-        turn_direction1(Complex::new(-1, 0), 270)
-    );
-    assert_eq!(
-        Complex::new(0, 1),
-        turn_direction1(Complex::new(-1, 0), -90)
-    );
+fn test_turn_direction() {
+    assert_eq!(Complex::new(0, -1), turn_direction(Complex::new(-1, 0), 90));
+    assert_eq!(Complex::new(1, 0), turn_direction(Complex::new(-1, 0), 180));
+    assert_eq!(Complex::new(0, 1), turn_direction(Complex::new(-1, 0), 270));
+    assert_eq!(Complex::new(0, 1), turn_direction(Complex::new(-1, 0), -90));
     assert_eq!(
         Complex::new(1, 0),
-        turn_direction1(Complex::new(-1, 0), -180)
-    );
-    assert_eq!(
-        Complex::new(-4, -10),
-        turn_direction1(Complex::new(-10, 4), 90)
+        turn_direction(Complex::new(-1, 0), -180)
     );
 }
 
--- src/day25.rs
+++ src/day25.rs
@@ -14,7 +14,10 @@ fn parse_input_and_find_encryption_key(inp: String) -> i32 {
 }
 
 fn parse_input(inp: String) -> (i32, i32) {
-    inp.lines().map(|l| l.parse().unwrap()).next_tuple().unwrap()
+    inp.lines()
+        .map(|l| l.parse().unwrap())
+        .next_tuple()
+        .unwrap()
 }
 
 fn find_encryption_key(public_key1: i32, public_key2: i32) -> i32 {
@@ -22,10 +25,14 @@ fn find_encryption_key(public_key1: i32, public_key2: i32) -> i32 {
     let mut current_value = 7;
     loop {
         if current_value == public_key1 {
-            return modpow(&public_key2, &current_loop_size, &M).to_i32().unwrap();
+            return modpow(&public_key2, &current_loop_size, &M)
+                .to_i32()
+                .unwrap();
         }
         if current_value == public_key2 {
-            return modpow(&public_key1, &current_loop_size, &M).to_i32().unwrap();
+            return modpow(&public_key1, &current_loop_size, &M)
+                .to_i32()
+                .unwrap();
         }
         current_loop_size += 1;
         current_value = (current_value * 7) % M;
@@ -34,7 +41,10 @@ fn find_encryption_key(public_key1: i32, public_key2: i32) -> i32 {
 
 #[test]
 fn test_parse_input_and_find_encryption_key() {
-    assert_eq!(14897079, parse_input_and_find_encryption_key(TEST_INPUT.to_string()));
+    assert_eq!(
+        14897079,
+        parse_input_and_find_encryption_key(TEST_INPUT.to_string())
+    );
 }
 
 #[allow(dead_code)]
